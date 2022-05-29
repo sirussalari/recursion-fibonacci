f169ed31a8e2847529a9e459f5258a1559bb682a
sirussalari <sirus.salari@gmail.com>
2022-05-29 00:00:06 -0700

    Update program to be object oriented

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,32 +1,3 @@
-#USING ITERATION
-def fibs(n)
-    range = n - 2
-    fib_arr = [0, 1]
-
-    for i in 0...range
-        preceeding_fib = fib_arr[fib_arr.length - 2]
-        current_fib = fib_arr[fib_arr.length - 1]
-        subsequent_fib = current_fib + preceeding_fib
-        fib_arr.push(subsequent_fib)
-    end
-
-    return fib_arr
-end
-
-#USING RECURSION
-def fibs_rec(n, fib_arr = [0, 1])
-    preceeding_fib = fib_arr[fib_arr.length - 2]
-    current_fib = fib_arr[fib_arr.length - 1]
-    subsequent_fib = preceeding_fib + current_fib
-    fib_arr.push(subsequent_fib)
-
-    if n > 3
-        fibs_rec(n - 1, fib_arr)
-    end
-
-    return fib_arr 
-end
-
 class Fibonacci
     @@fib_arr = [0, 1]
 
